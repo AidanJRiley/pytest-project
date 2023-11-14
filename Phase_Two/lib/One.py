04d554ed9bbad62bdbdf91f6ace1f9973574acc5
AnnGalloway <60460582+AnnGalloway@users.noreply.github.com>
2023-11-14 10:15:08 +0000

    Phase Two Exercise One refactored

--- Phase_Two/lib/One.py
+++ Phase_Two/lib/One.py
@@ -1,20 +1,23 @@
 def estimate_reading_time(text):
     seconds = round(len(text.split()) /200 * 60)
+    
     if seconds < 60:
         if seconds == 1:    
             return f"{seconds} second"
         return f"{seconds} seconds"
     remainder = seconds % 60
     minutes = round((seconds - remainder)/60)
-    if minutes == 1 and remainder > 1:
-        return f"{minutes} minute {remainder} seconds"
-    if minutes == 1 and remainder == 0:
-        return f"{minutes} minute"
-    if minutes == 1 and remainder == 1:
-        return f"{minutes} minute {remainder} second"
-    if minutes > 1 and remainder > 1:
-        return f"{minutes} minutes {remainder} seconds"
-    if minutes > 1 and remainder == 0:
-        return f"{minutes} minutes"
-    if minutes > 1 and remainder == 1:
-        return f"{minutes} minutes {remainder} second"
+    
+    if minutes > 1:
+        minutes_result = f"{minutes} minutes"
+    elif minutes == 1:
+        minutes_result = f"{minutes} minute"
+    
+    if remainder > 1:
+        seconds_result = f"{remainder} seconds"
+    elif remainder == 1:
+        seconds_result = f"{remainder} second"
+    
+    if remainder == 0:
+        return minutes_result
+    return f"{minutes_result} {seconds_result}"
